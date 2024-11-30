5290d3513c349408d9f6a4b3daf8d593f6aaa8af
AkhiJAIN <vanakundreakhilesh@gamil.com>
2024-11-30 17:25:41 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Student Chatbot',
+    version= '0.0.0',
+    author= 'Akhi jain',
+    author_email= 'vavnkundreakhilesh@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
